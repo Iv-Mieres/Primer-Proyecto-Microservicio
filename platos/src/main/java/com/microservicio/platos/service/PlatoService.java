9e905a5a4c38ac21c403bcdce03d70108c18530c
Ivan Mieres <mieres.iv@gmail.com>
2023-09-23 00:03:05 -0300

    add: configuration, controller, dto and service layer

--- platos/src/main/java/com/microservicio/platos/service/PlatoService.java
+++ platos/src/main/java/com/microservicio/platos/service/PlatoService.java
@@ -1,9 +1,15 @@
 package com.microservicio.platos.service;
 
+import java.util.ArrayList;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.microservicio.platos.dto.IngredienteDTO;
 import com.microservicio.platos.dto.PlatoDTO;
+import com.microservicio.platos.model.Plato;
 import com.microservicio.platos.repository.IPlatoRepository;
 
 @Service
@@ -12,15 +18,29 @@ public class PlatoService implements IPlatoService{
 	@Autowired
 	private IPlatoRepository platoRepository;
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 
 	@Override
 	public void savePlato(PlatoDTO plato) {
+		String endpoint = "http://localhost:9002/ingredientes/"; 
+		var guardarPlato = modelMapper.map(plato, Plato.class);
+		var nombreIngredientes = new ArrayList<String>();
+		
+		for (IngredienteDTO ingrediente: restTemplate.getForObject(endpoint + plato.getNombre(), IngredienteDTO[].class)) {
+			nombreIngredientes.add(ingrediente.getNombreIngrediente());
+		}		
+		guardarPlato.setIngredientes(nombreIngredientes);
+		platoRepository.save(guardarPlato);			
 	}
 
 	@Override
 	public PlatoDTO verPlatoPorId(Long id) {
-		
-		return null;
+		return  modelMapper.map(platoRepository.findById(id), PlatoDTO.class);
 	}
 
 	@Override
